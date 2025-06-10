35964407ed3c8f55cb58c5403853b64fcd5444b7
Tomohide <tomohide.monoi@gmail.com>
2025-06-10 16:32:33 +0900

    Updated

--- cdk/lib/social-listening-stack.ts
+++ cdk/lib/social-listening-stack.ts
@@ -7,12 +7,10 @@ export class SocialListeningStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Docker イメージをビルドして ECR アセットに登録
     const image = new ecr_assets.DockerImageAsset(this, 'AppImage', {
-      directory: '../',  // ルートの Dockerfile を参照
+      directory: path.join(__dirname, '../../'),
     });
 
-    // App Runner サービスを作成
     new apprunner.CfnService(this, 'AppRunnerService', {
       serviceName: 'social-listening',
       sourceConfiguration: {
